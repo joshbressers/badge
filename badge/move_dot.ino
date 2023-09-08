9915742a85af98d3b3405ab9c6e5ac443790c983
Josh Bressers <josh@bress.net>
2023-09-07 20:13:35 -0500

    Use uint8_t instead of float
    Signed-off-by: Josh Bressers <josh@bress.net>

--- badge/move_dot.ino
+++ badge/move_dot.ino
@@ -1,22 +1,22 @@
 
 #include "constants.h"
 
-float x = 4;
-float y = 4;
-float x_vel = 0;
-float y_vel = 0;
+uint8_t x = 4 * 32;
+uint8_t y = 4 * 32;
+uint8_t x_vel = 0;
+uint8_t y_vel = 0;
 
 void moveDot() {
   clearFrameBuffer();
   
-  if (NEW_BUTTON(BTN_RIGHT)) x_vel += 0.1;
-  if (NEW_BUTTON(BTN_DOWN)) y_vel += 0.1;
-  if (NEW_BUTTON(BTN_UP)) y_vel -= 0.1;
-  if (NEW_BUTTON(BTN_LEFT)) x_vel -= 0.1;
+  if (NEW_BUTTON(BTN_RIGHT)) x_vel++;
+  if (NEW_BUTTON(BTN_DOWN)) y_vel++;
+  if (NEW_BUTTON(BTN_UP)) y_vel--;
+  if (NEW_BUTTON(BTN_LEFT)) x_vel--;
 
   if (NEW_BUTTON(BTN_A) || NEW_BUTTON(BTN_B)) {
-    x = 4;
-    y = 4;
+    x = 4 * 32;
+    y = 4 * 32;
     x_vel = 0;
     y_vel = 0;
   }
@@ -24,11 +24,6 @@ void moveDot() {
   x = x + x_vel;
   y = y + y_vel;
 
-  if (x > 7) x = 0;
-  if (y > 7) y = 0;
-  if (x < 0) x = 7;
-  if (y < 0) y = 7;
-
-  frameBuffer[(int)x] = 1 << (int)y;
+  frameBuffer[x/32] = 1 << (y/32);
 
 }
